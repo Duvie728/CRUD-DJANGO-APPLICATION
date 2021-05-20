9a5603c3094711208393a52d4c1f7fed09925b3e
Duvie <80975277+Duvie728@users.noreply.github.com>
2021-05-20 18:02:05 +0100

    rectifying further error

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm 
 from django.urls import reverse_lazy
-from django.views import generic 
+#from django.views import generic 
+from django.views.generic import CreateView
 
 # Create your views here.
-class SignUpView(generic.CreateView):
+#class SignUpView(generic.CreateView):
+class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
